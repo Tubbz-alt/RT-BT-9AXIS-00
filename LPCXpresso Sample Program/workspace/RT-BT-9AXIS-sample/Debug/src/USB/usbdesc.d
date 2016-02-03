5b2588d64f4e7a76bc0b1962ed436528c664b156
taryo <r.takahashi@rt-net.jp>
2016-02-03 12:49:36 +0900

    first comit

---
+++ LPCXpresso Sample Program/workspace/RT-BT-9AXIS-sample/Debug/src/USB/usbdesc.d
@@ -0,0 +1,19 @@
+src/USB/usbdesc.o src/USB/usbdesc.d: ../src/USB/usbdesc.c \
+ C:\Users\ryota\OneDrive\RT\ working\RT-BT-9AXIS-00\lpcXpresso\workspace\RT-BT-9AXIS-sample\inc\OTHER/type.h \
+ C:\Users\ryota\OneDrive\RT\ working\RT-BT-9AXIS-00\lpcXpresso\workspace\RT-BT-9AXIS-sample\inc\USB/usb.h \
+ C:\Users\ryota\OneDrive\RT\ working\RT-BT-9AXIS-00\lpcXpresso\workspace\RT-BT-9AXIS-sample\inc\USB/cdc.h \
+ C:\Users\ryota\OneDrive\RT\ working\RT-BT-9AXIS-00\lpcXpresso\workspace\RT-BT-9AXIS-sample\inc\USB/usbcfg.h \
+ C:\Users\ryota\OneDrive\RT\ working\RT-BT-9AXIS-00\lpcXpresso\workspace\RT-BT-9AXIS-sample\inc\USB/usbdesc.h \
+ C:\Users\ryota\OneDrive\RT\ working\RT-BT-9AXIS-00\lpcXpresso\workspace\RT-BT-9AXIS-sample\inc\USB/config.h
+
+C:\Users\ryota\OneDrive\RT\ working\RT-BT-9AXIS-00\lpcXpresso\workspace\RT-BT-9AXIS-sample\inc\OTHER/type.h:
+
+C:\Users\ryota\OneDrive\RT\ working\RT-BT-9AXIS-00\lpcXpresso\workspace\RT-BT-9AXIS-sample\inc\USB/usb.h:
+
+C:\Users\ryota\OneDrive\RT\ working\RT-BT-9AXIS-00\lpcXpresso\workspace\RT-BT-9AXIS-sample\inc\USB/cdc.h:
+
+C:\Users\ryota\OneDrive\RT\ working\RT-BT-9AXIS-00\lpcXpresso\workspace\RT-BT-9AXIS-sample\inc\USB/usbcfg.h:
+
+C:\Users\ryota\OneDrive\RT\ working\RT-BT-9AXIS-00\lpcXpresso\workspace\RT-BT-9AXIS-sample\inc\USB/usbdesc.h:
+
+C:\Users\ryota\OneDrive\RT\ working\RT-BT-9AXIS-00\lpcXpresso\workspace\RT-BT-9AXIS-sample\inc\USB/config.h:
